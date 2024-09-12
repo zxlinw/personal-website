6d9e03ce7857cc8a683997dfc35162bfb0c8ac81
Zilin <hannahzweng@gmail.com>
2024-09-12 13:15:21 -0400

    image 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/personal-website' : '',
     output: 'export',
     distDir: 'dist',
     images: {
